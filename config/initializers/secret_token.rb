ee078afe3385177e4ba7dc879062ac16f7e5999d
Steve Dodkin <stevedodkin@mac.com>
2012-09-27 10:05:11 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HLOTSSite::Application.config.secret_token = 'a5f99e073081b5cd0b1c0353204f0403b557dd4618b9828661f7f06607a7cf5674d0eb522d807d1a47c17fa9fcfa87a5378b1a44de88414fbe641040563147b4'
